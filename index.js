b24f4c0666cdba965935849660c4f252c209b211
Chris Young <chrisyoung0101@gmail.com>
2021-07-24 20:23:53 -0500

    finish mercury content styling work on menu show hode

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello from JS.');
+// menu-btn
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuBtnShow = document.querySelector('.show');
+const menu = document.querySelector('nav ul');
+
+menuBtn.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0)';
+  menuBtn.classList.add('show');
+});
+
+menuBtnShow.addEventListener('click', () => {
+  menu.style.transform = 'translateX(100%)';
+});
