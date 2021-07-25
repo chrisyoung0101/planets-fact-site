63a5f528b7b1a7f69a85d6af7889198c4a5fefea
Chris Young <chrisyoung0101@gmail.com>
2021-07-25 11:47:06 -0500

    transition in & out functionality complete

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const menuBtnShow = document.querySelector('.show');
 const menu = document.querySelector('nav ul');
 
 menuBtn.addEventListener('click', () => {
-  menu.style.transform = 'translateX(0)';
-  menuBtn.classList.add('show');
-});
+  // getting the matrix value of translateX to toggle the menu
 
-menuBtnShow.addEventListener('click', () => {
-  menu.style.transform = 'translateX(100%)';
+  const style = window.getComputedStyle(menu);
+  //this is a 2d matrix
+  //https://zellwk.com/blog/css-translate-values-in-javascript/
+  const matrix = style.transform || style.webkitTransform || style.mozTransform;
+
+  //split up the matrix into an array?
+  const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ');
+
+  //   4 = translateX value
+  //   5 = translateY value
+  const x = matrixValues[4];
+
+  // console.log(theCSSprop);
+
+  if (x > 0) {
+    menu.style.transform = 'translateX(0)';
+  } else {
+    menu.style.transform = 'translateX(100%)';
+  }
 });
