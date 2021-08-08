68c68d2d8490598fc34ac6ce3725acdd7f1e6ce2
Chris Young <chrisyoung0101@gmail.com>
2021-08-08 16:06:25 -0500

    add default and select behavior to side-btns

--- index.js
+++ index.js
@@ -43,3 +43,94 @@ menuBtn.addEventListener('click', () => {
     infotangle.style.display = 'block';
   }
 });
+
+// ************* MOBILE **************
+//below js : most likely this could be simpler but...
+
+//.myelement is being added to the overview button by default
+//when another button is clicked this default is removed and that button now has the :after bottom border
+
+const overview = document.querySelector('.overview');
+const structure = document.querySelector('.structure');
+const surface = document.querySelector('.surface');
+
+//default
+overview.classList.add('myelement');
+
+const clicked = false;
+structure.addEventListener('click', (clicked) => {
+  clicked = true;
+  if (clicked) {
+    structure.classList.add('myelement');
+    overview.classList.remove('myelement');
+    surface.classList.remove('myelement');
+  }
+});
+
+surface.addEventListener('click', (clicked) => {
+  clicked = true;
+  if (clicked) {
+    surface.classList.add('myelement');
+    overview.classList.remove('myelement');
+    structure.classList.remove('myelement');
+  }
+});
+
+overview.addEventListener('click', (clicked) => {
+  clicked = true;
+  if (clicked) {
+    overview.classList.add('myelement');
+    surface.classList.remove('myelement');
+    structure.classList.remove('myelement');
+  }
+});
+
+var structureIsClicked = false;
+var surfaceIsClicked = false;
+function clickHandler(event) {
+  structureIsClicked = true;
+  surfaceIsClicked = true;
+  if (structureIsClicked || surfaceIsClicked) {
+    overview.classList.remove('myelement');
+  }
+}
+
+// ************* MOBILE **************
+
+const sideBtn1 = document.querySelector('.side-btn-1');
+const sideBtn2 = document.querySelector('.side-btn-2');
+const sideBtn3 = document.querySelector('.side-btn-3');
+
+//default
+sideBtn1.classList.add('myBackGroundColor');
+
+sideBtn1.classList.remove('side-btn-1:hover');
+
+const clickedSide = false;
+
+sideBtn1.addEventListener('click', (clickedSide) => {
+  clickedSide = true;
+  if (clickedSide) {
+    sideBtn1.classList.add('myBackGroundColor');
+    sideBtn2.classList.remove('myBackGroundColor');
+    sideBtn3.classList.remove('myBackGroundColor');
+  }
+});
+
+sideBtn2.addEventListener('click', (clickedSide) => {
+  clickedSide = true;
+  if (clickedSide) {
+    sideBtn1.classList.remove('myBackGroundColor');
+    sideBtn2.classList.add('myBackGroundColor');
+    sideBtn3.classList.remove('myBackGroundColor');
+  }
+});
+
+sideBtn3.addEventListener('click', (clickedSide) => {
+  clickedSide = true;
+  if (clickedSide) {
+    sideBtn1.classList.remove('myBackGroundColor');
+    sideBtn2.classList.remove('myBackGroundColor');
+    sideBtn3.classList.add('myBackGroundColor');
+  }
+});
